e937197fade3628f3de91d137bd9df5f53b90bed
Nikolas <131282424+N1k0145@users.noreply.github.com>
2025-02-06 16:23:13 -0300

    Add files via upload

---
+++ projetoweb/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Exemplo 02</title>
+        <body>
+            <?php
+                echo "Ola mundo que usa PHP, ESSE E O PRIMEIRO PROGRAMA!"."<br>"; 
+                echo "Espero aprender bastante sobre essa poderosa linguagem"."<br>";
+                echo "durante as aulas";
+            ?>
+        </body>
+    </head>
+</html>
