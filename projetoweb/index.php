84a1d039ee1db1b8a9cdc15b3d943daee51d5b02
Nikolas <131282424+N1k0145@users.noreply.github.com>
2025-02-06 16:24:34 -0300

    Add files via upload

--- projetoweb/index.php
+++ projetoweb/index.php
@@ -3,10 +3,54 @@
         <title>Exemplo 02</title>
         <body>
             <?php
-                echo "Ola mundo que usa PHP, ESSE E O PRIMEIRO PROGRAMA!"."<br>"; 
-                echo "Espero aprender bastante sobre essa poderosa linguagem"."<br>";
-                echo "durante as aulas";
-            ?>
+           //     echo "Ola mundo que usa PHP, ESSE E O PRIMEIRO PROGRAMA!"."<br>"; 
+             //   echo "Espero aprender bastante sobre essa poderosa linguagem"."<br>";
+               // echo "durante as aulas";
+            //?>
         </body>
     </head>
 </html>
+
+<html>
+<head>
+ <title>PHP – Aula Exemplo do Uso de Variáveis</title>
+ </head>
+ <body>
+ <?php
+ // Declarando variáveis
+ $var1 = 1; // inteiro
+ $varX = 9.50; // ponto flutuante
+ $varY = 9.50; // ponto flutuante
+$var2 = $varX + $varY; //recebendo o resultado de uma
+expressão
+ $varSTR = "Atribui String"; // variável texto – string
+ $varletra = "a"; // variável caracter
+ $varbool = true; // atribuição lógica
+ echo "Exemplo de uma aplicação em PHP";
+ print "<P>";
+ echo "Conteúdo de var1: ";
+ echo $var1;
+ print "<P>";
+ echo "Conteúdo de varX: ";
+ echo $varX;
+ print "<P>";
+
+ echo "Conteúdo de varY: ";
+ echo $varY;
+ print "<P>";
+ echo "Conteúdo de var2: ";
+ echo $var2;
+ print "<P>";
+
+ echo "Conteúdo de varSTR: ";
+ echo $varSTR;
+ print "<P>";
+
+ echo "Conteúdo de varletra: ";
+ echo $varletra;
+ print "<P>";
+ echo "Conteúdo de varbool: ";
+ echo $varbool;
+ ?>
+ </body>
+ </html>
